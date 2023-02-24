07d214dea8369a0f7f69a29920e5f56f0319897c
Afolabi <opakunleafolabi@gmail.com>
2023-02-25 00:11:25 +0100

    image styled

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  user$ = this.authService.currentUser$;
+
+  constructor(
+    private authService: AuthService,
+              ) {}
 }
